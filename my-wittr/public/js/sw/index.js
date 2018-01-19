476afe33c57b3efac6576afb5032323fc66d4361
Jordan Marshall <jordan.marshall@acadiahealthcare.com>
2018-01-19 16:03:16 -0600

    return cached response if exists

--- my-wittr/public/js/sw/index.js
+++ my-wittr/public/js/sw/index.js
@@ -1,15 +1,27 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('wittr-static-v1').then(function (cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  // TODO: respond with an entry from the cache if there is one.
+  // If there isn't, fetch from the network.
   event.respondWith(
-    fetch(event.request).then(function(response) {
-      if (response.status === 404) {
-        // TODO: instead, respond with the gif at
-        // /imgs/dr-evil.gif
-        // using a network request
-        return fetch('imgs/dr-evil.gif');
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
       }
-      return response;
-    }).catch(function() {
-      return new Response("Uh oh, that totally failed!");
+      return fetch(event.request);
     })
   );
 });
