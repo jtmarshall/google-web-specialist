2120452e9e8af5a1d652cd4a63a5d1fda9a98c47
Jordan Marshall <jordan.marshall@acadiahealthcare.com>
2018-01-19 14:20:57 -0600

    hijacking jpg requests

--- my-wittr/public/js/sw/index.js
+++ my-wittr/public/js/sw/index.js
@@ -1,13 +1,11 @@
-self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
+self.addEventListener('fetch', function (event) {
+  // TODO: respond to all requests with an html response
+  // containing an element with class="a-winner-is-me".
   // Ensure the Content-Type of the response is "text/html"
-  event.respondWith(
-    new Response('<h2 class="a-winner-is-me">Winner I is!</h2>', {
-      headers: {
-        'Content-Type': 'text/html'
-      }
-    })
-  );
-  console.log(event.request);
+  if (event.request.url.endsWith('.jpg')) {
+    event.respondWith(
+      fetch('imgs/dr-evil.gif')
+    );
+  }
+  // console.log(event.request);
 });
