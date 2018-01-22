cf23d49e4f997f7e4278e8af28851b02fbea334b
Jordan Marshall <jordan.marshall@acadiahealthcare.com>
2018-01-22 14:21:51 -0600

    auto cleaning IDB

--- my-wittr/public/js/idb-test/index.js
+++ my-wittr/public/js/idb-test/index.js
@@ -1,14 +1,12 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 4, function (upgradeDb) {
-  switch (upgradeDb.oldVersion) {
+var dbPromise = idb.open('test-db', 4, function(upgradeDb) {
+  switch(upgradeDb.oldVersion) {
     case 0:
       var keyValStore = upgradeDb.createObjectStore('keyval');
       keyValStore.put("world", "hello");
     case 1:
-      upgradeDb.createObjectStore('people', {
-        keyPath: 'name'
-      });
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
     case 2:
       var peopleStore = upgradeDb.transaction.objectStore('people');
       peopleStore.createIndex('animal', 'favoriteAnimal');
@@ -19,35 +17,35 @@ var dbPromise = idb.open('test-db', 4, function (upgradeDb) {
 });
 
 // read "hello" in "keyval"
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('keyval');
   var keyValStore = tx.objectStore('keyval');
   return keyValStore.get('hello');
-}).then(function (val) {
+}).then(function(val) {
   console.log('The value of "hello" is:', val);
 });
 
 // set "foo" to be "bar" in "keyval"
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('keyval', 'readwrite');
   var keyValStore = tx.objectStore('keyval');
   keyValStore.put('bar', 'foo');
   return tx.complete;
-}).then(function () {
+}).then(function() {
   console.log('Added foo:bar to keyval');
 });
 
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('keyval', 'readwrite');
   var keyValStore = tx.objectStore('keyval');
   keyValStore.put('cat', 'favoriteAnimal');
   return tx.complete;
-}).then(function () {
+}).then(function() {
   console.log('Added favoriteAnimal:cat to keyval');
 });
 
 // add people to "people"
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('people', 'readwrite');
   var peopleStore = tx.objectStore('people');
 
@@ -76,40 +74,40 @@ dbPromise.then(function (db) {
   });
 
   return tx.complete;
-}).then(function () {
+}).then(function() {
   console.log('People added');
 });
 
 // list all cat people
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('people');
   var peopleStore = tx.objectStore('people');
   var animalIndex = peopleStore.index('animal');
 
   return animalIndex.getAll('cat');
-}).then(function (people) {
+}).then(function(people) {
   console.log('Cat people:', people);
 });
 
 // people by age
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('people');
   var peopleStore = tx.objectStore('people');
   var ageIndex = peopleStore.index('age');
 
   return ageIndex.getAll();
-}).then(function (people) {
+}).then(function(people) {
   console.log('People by age:', people);
 });
 
 // Using cursors
-dbPromise.then(function (db) {
+dbPromise.then(function(db) {
   var tx = db.transaction('people');
   var peopleStore = tx.objectStore('people');
   var ageIndex = peopleStore.index('age');
 
   return ageIndex.openCursor();
-}).then(function (cursor) {
+}).then(function(cursor) {
   if (!cursor) return;
   return cursor.advance(2);
 }).then(function logPerson(cursor) {
@@ -119,6 +117,6 @@ dbPromise.then(function (db) {
   // cursor.update(newValue) to change the value, or
   // cursor.delete() to delete this entry
   return cursor.continue().then(logPerson);
-}).then(function () {
+}).then(function() {
   console.log('Done cursoring');
 });
--- my-wittr/public/js/main/IndexController.js
+++ my-wittr/public/js/main/IndexController.js
@@ -9,12 +9,7 @@ function openDatabase() {
     return Promise.resolve();
   }
 
-  // TODO: return a promise for a database called 'wittr'
-  // that contains one objectStore: 'wittrs'
-  // that uses 'id' as its key
-  // and has an index called 'by-date', which is sorted
-  // by the 'time' property
-  return idb.open('wittr', 1, function (upgradeDb) {
+  return idb.open('wittr', 1, function(upgradeDb) {
     var store = upgradeDb.createObjectStore('wittrs', {
       keyPath: 'id'
     });
@@ -27,17 +22,22 @@ export default function IndexController(container) {
   this._postsView = new PostsView(this._container);
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
-  this._openSocket();
   this._dbPromise = openDatabase();
   this._registerServiceWorker();
+
+  var indexController = this;
+
+  this._showCachedMessages().then(function() {
+    indexController._openSocket();
+  });
 }
 
-IndexController.prototype._registerServiceWorker = function () {
+IndexController.prototype._registerServiceWorker = function() {
   if (!navigator.serviceWorker) return;
 
   var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function (reg) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
     if (!navigator.serviceWorker.controller) {
       return;
     }
@@ -52,7 +52,7 @@ IndexController.prototype._registerServiceWorker = function () {
       return;
     }
 
-    reg.addEventListener('updatefound', function () {
+    reg.addEventListener('updatefound', function() {
       indexController._trackInstalling(reg.installing);
     });
   });
@@ -60,37 +60,53 @@ IndexController.prototype._registerServiceWorker = function () {
   // Ensure refresh is only called once.
   // This works around a bug in "force update on reload".
   var refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', function () {
+  navigator.serviceWorker.addEventListener('controllerchange', function() {
     if (refreshing) return;
     window.location.reload();
     refreshing = true;
   });
 };
 
-IndexController.prototype._trackInstalling = function (worker) {
+IndexController.prototype._showCachedMessages = function() {
   var indexController = this;
-  worker.addEventListener('statechange', function () {
+
+  return this._dbPromise.then(function(db) {
+    // if we're already showing posts, eg shift-refresh
+    // or the very first load, there's no point fetching
+    // posts from IDB
+    if (!db || indexController._postsView.showingPosts()) return;
+
+    var index = db.transaction('wittrs')
+      .objectStore('wittrs').index('by-date');
+
+    return index.getAll().then(function(messages) {
+      indexController._postsView.addPosts(messages.reverse());
+    });
+  });
+};
+
+IndexController.prototype._trackInstalling = function(worker) {
+  var indexController = this;
+  worker.addEventListener('statechange', function() {
     if (worker.state == 'installed') {
       indexController._updateReady(worker);
     }
   });
 };
 
-IndexController.prototype._updateReady = function (worker) {
+IndexController.prototype._updateReady = function(worker) {
   var toast = this._toastsView.show("New version available", {
     buttons: ['refresh', 'dismiss']
   });
 
-  toast.answer.then(function (answer) {
+  toast.answer.then(function(answer) {
     if (answer != 'refresh') return;
-    worker.postMessage({
-      action: 'skipWaiting'
-    });
+    worker.postMessage({action: 'skipWaiting'});
   });
 };
 
 // open a connection to the server for live updates
-IndexController.prototype._openSocket = function () {
+IndexController.prototype._openSocket = function() {
   var indexController = this;
   var latestPostDate = this._postsView.getLatestPostDate();
 
@@ -109,45 +125,51 @@ IndexController.prototype._openSocket = function () {
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function () {
+  ws.addEventListener('open', function() {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function (event) {
-    requestAnimationFrame(function () {
+  ws.addEventListener('message', function(event) {
+    requestAnimationFrame(function() {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function () {
+  ws.addEventListener('close', function() {
     // tell the user
     if (!indexController._lostConnectionToast) {
       indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
     }
 
     // try and reconnect in 5 seconds
-    setTimeout(function () {
+    setTimeout(function() {
       indexController._openSocket();
     }, 5000);
   });
 };
 
 // called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function (data) {
+IndexController.prototype._onSocketMessage = function(data) {
   var messages = JSON.parse(data);
 
-  this._dbPromise.then(function (db) {
+  this._dbPromise.then(function(db) {
     if (!db) return;
 
-    // TODO: put each message into the 'wittrs'
-    // object store.
     var tx = db.transaction('wittrs', 'readwrite');
-    var wittrStore = tx.objectStore('wittrs');
-    messages.forEach(function (message) {
-      wittrStore.put(message);
-      return tx.complete;
+    var store = tx.objectStore('wittrs');
+    messages.forEach(function(message) {
+      store.put(message);
+    });
+
+    // limit store to 30 items
+    store.index('by-date').openCursor(null, "prev").then(function(cursor) {
+      return cursor.advance(30);
+    }).then(function deleteRest(cursor) {
+      if (!cursor) return;
+      cursor.delete();
+      return cursor.continue().then(deleteRest);
     });
   });
 
